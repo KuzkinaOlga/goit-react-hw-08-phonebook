bf783cbac34709100aa06f0bf5f8e8a42e967846
KuzkinaOlga <o.kuzkina@gmail.com>
2022-07-23 14:01:11 +0200

    hw react 3.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,38 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    //     this.setState(prevState=>{
+    // return {...prevState, filter: prevState.filter }
+    //     }
+    //     )
+    const getStorage = JSON.parse(localStorage.getItem('contacts'));
+
+    if (getStorage) {
+      return this.setState({
+        contacts: getStorage,
+      });
+    } else {
+      return this.setState({
+        contacts: [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   visibleContacts = () => {
     const { contacts, filter } = this.state;
