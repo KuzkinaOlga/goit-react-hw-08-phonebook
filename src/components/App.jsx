807e66770cbe13de886ecb2225705a7050fa98c7
KuzkinaOlga <o.kuzkina@gmail.com>
2022-08-03 14:16:26 +0200

    hw react 4.2 fied

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,95 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './Container/Container';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const getStorage = JSON.parse(localStorage.getItem('contacts'));
+const App = () => {
+  const [contacts, setContacts] = useState(() =>
+    localStorage.getItem('contacts')
+      ? JSON.parse(localStorage.getItem('contacts'))
+      : initialContacts
+  );
+  const [filter, setFilter] = useState('');
 
-    if (getStorage) {
-      this.setState({
-        contacts: getStorage,
-      });
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  visibleContacts = () => {
-    const { contacts, filter } = this.state;
-
+  const visibleContacts = () => {
     const normaliseLowerCase = filter.toLowerCase();
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normaliseLowerCase)
     );
   };
-  deliteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+
+  const deliteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  newContact = contact => {
-    this.setState(({ contacts }) => {
-      const { name, number } = contact;
-
-      if (
-        contacts.some(
-          contact =>
-            contact.name.toLowerCase() === name.toLowerCase() ||
-            contact.number === number
-        )
-      ) {
-        return alert(`this name or number is already in contacts`);
-      }
-
-      return { contacts: [contact, ...contacts] };
+  const addContact = newContact => {
+    if (
+      contacts.some(contact => {
+        return (
+          contact.name.toLowerCase() === newContact.name.toLowerCase() ||
+          contact.number === newContact.number
+        );
+      })
+    ) {
+      return alert(`this name or number is already in contacts`);
+    }
+    setContacts(prevContacts => {
+      return [newContact, ...prevContacts];
     });
   };
-  formSubmitFilter = element => {
-    this.setState({ filter: element.currentTarget.value });
+  const setFilterValue = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  render() {
-    return (
-      <>
-        <Container>
-          <h1>Phonebook</h1>
-          <ContactForm newContact={this.newContact} />
+  return (
+    <>
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm newContact={addContact} />
 
-          <h2>Contacts</h2>
-          <Filter value={this.filter} onChange={this.formSubmitFilter} />
-          {this.state.contacts.length ? (
-            <ContactList
-              contacts={this.visibleContacts()}
-              delite={this.deliteContact}
-            />
-          ) : (
-            <h2>Your phonebook is empty !!!</h2>
-          )}
-        </Container>
-      </>
-    );
-  }
-}
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={setFilterValue} />
+        {contacts.length ? (
+          <ContactList contacts={visibleContacts()} delite={deliteContact} />
+        ) : (
+          <h2>Your phonebook is empty !!!</h2>
+        )}
+      </Container>
+    </>
+  );
+};
 
 App.propTypes = {
   contacts: PropTypes.arrayOf(
