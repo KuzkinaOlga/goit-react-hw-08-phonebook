1f10528ae6bf7d3878bc79ebb6edb8f1261e4b7b
KuzkinaOlga <o.kuzkina@gmail.com>
2022-09-07 22:32:30 +0200

    hw react 8 added stelrs2

--- src/components/RegNav.js
+++ src/components/RegNav.js
@@ -1,4 +1,4 @@
-import { Button, IconButton } from '@mui/material';
+import { Button } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import Fingerprint from '@mui/icons-material/Fingerprint';
 
